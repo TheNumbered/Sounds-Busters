c3097b2e653a70f3eb83f226ac9dac8ae8783b70
TheNumbered <2556833@students.wits.ac.za>
2024-10-19 10:46:27 +0200

    feat: initilised vite frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const App: React.FC = () => {
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>React + TypeScript</h1>
+            </header>
+        </div>
+    );
+}
+
+export default App;
