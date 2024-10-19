147323fac60dc15eaf438af3aec97a457d0c6409
TheNumbered <2556833@students.wits.ac.za>
2024-10-19 10:42:33 +0200

    feat: initilised backend

---
+++ server/app.js
@@ -0,0 +1,20 @@
+import cors from 'cors';
+import express from 'express';
+
+const app = express();
+
+// Enable CORS and JSON middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello, world!' });
+});
+
+// Error handling
+app.use((req, res, next) => {
+  res.status(500).json({ message: 'Internal server error' });
+  next();
+});
+
+export default app;
